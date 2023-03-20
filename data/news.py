a504c2c06c01b7ca52d572d4f9170f2cbb3e3acf
TPLV <m4xlion@yandex.ru>
2023-03-20 21:28:14 +0300

    WEB. Знакомство с flask-sqlalchemy Форма Регестрации

--- data/news.py
+++ data/news.py
@@ -16,6 +16,10 @@ class News(SqlAlchemyBase):
                                      default=datetime.datetime.now)
     is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
 
+    categories = orm.relationship("Category",
+                                  secondary="association",
+                                  backref="news")
+
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
                                 sqlalchemy.ForeignKey("users.id"))
     user = orm.relationship('User')
